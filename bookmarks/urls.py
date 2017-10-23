2773fc1aa7c14a5f24bd7f4becc8f36c6d468cfb
Du Peng <dupeng83@outlook.com>
2017-10-23 19:43:42 +0800

    Add a root page

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^account/', include('account.urls')),
+    url(r'', include('account.urls')),
     url(r'^images/', include('images.urls', namespace='images')),
 ]
 
